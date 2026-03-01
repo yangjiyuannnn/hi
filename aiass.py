4ab4b6f9817cc648f0a77b63b1b3002a7be2ae35
yangjiyuannnn <yangjiyuan05@gmail.com>
2026-03-01 16:02:49 +0800

    Update aiass.py

--- aiass.py
+++ aiass.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1GG-vAiZGvFecbG7SwO58cLhvDafRvzd7
 """
 
-pip install nltk
+!pip install nltk
 
 !pip install streamlit
 # Import basic libraries
